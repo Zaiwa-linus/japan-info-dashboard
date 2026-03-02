6f76fac069372d96453b6cc50ffffaecc29c3305
Mitsuhiro <mitsuhiro.n.0919@gmail.com>
2026-03-02 20:29:05 +0900

    耐久消費財データモデルとSQLクエリを追加

---
+++ dbt_project/models/intermediate/int_durable_goods.sql
@@ -0,0 +1,14 @@
+-- 耐久消費財所有数量（二人以上の世帯）の都道府県別データ
+-- 時代の変遷が見える家電・自動車・デジタル機器などの普及状況
+
+select
+    indicator_code,
+    indicator_name,
+    area_code,
+    area_name,
+    cast(left(cast(year_code as varchar), 4) as integer) as year,
+    unit,
+    value
+from {{ ref('stg_household') }}
+where indicator_code like 'L51%'
+    and area_name != '全国'
